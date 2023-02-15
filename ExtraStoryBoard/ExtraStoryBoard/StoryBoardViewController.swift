0a4140054e0fbe3cb5ebe968c28befa59b5028c1
Darshan Dangar <darshanahir57267@gmail.com>
2023-02-15 15:16:03 +0530

    Chapter1 task

---
+++ ExtraStoryBoard/ExtraStoryBoard/StoryBoardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StoryBoardViewController.swift
+//  ExtraStoryBoard
+//
+//  Created by Darshan Dangar on 15/02/23.
+//
+
+import UIKit
+
+class StoryBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        overrideUserInterfaceStyle = .dark
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
